3b8959f893950010978ec804825b0f9282eacc89
Ravi Gangwar <113000419+ravi-gangwar@users.noreply.github.com>
2024-01-18 07:00:14 +0000

    "learning String"

---
+++ JS/String.js
@@ -0,0 +1,15 @@
+const name_1 = "Ravi"; // define a String Variable 
+console.log(name_1)
+console.log(name_1.toLowerCase());
+
+const name_2 = new String("Ravi Gangwar") // second method of String this method also have __proto__
+console.log(name_2)
+console.log(name_2.trim())
+
+console.log(name_2.concat('2'))
+console.log(name_2.endsWith('r'))
+console.log(name_2.repeat(3))
+console.log(name_2.split(""))
+console.log(name_2.substring())
+console.log(name_2.includes('r'))
+console.log(name_2.lastIndexOf('a'))
